c2fec926a7fd998dd577efaa283b0c1a5d557cd7
vladi <vladislavskiyproduction@gmail.com>
2022-12-20 11:00:57 +0200

    added get test

--- src/test/java/com/vladislavskiy/eva/EvaApplicationTests.java
+++ src/test/java/com/vladislavskiy/eva/EvaApplicationTests.java
@@ -1,13 +1,27 @@
 package com.vladislavskiy.eva;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class EvaApplicationTests {
-
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void ExpectStatusIsOk()throws Exception
+	{
+		this.mockMvc.perform(get("/shop/products?nameFilter=")).andDo(print()).andExpect(status().isOk());
 	}
 
 }
